b0047955b252bf6d34cc1145ae130337be8787cf
Chase <72784986+aerglos@users.noreply.github.com>
2021-01-26 21:44:00 -0800

    Initial

---
+++ src/xyz/chasew/jacobsmmo/JacobsMMO.java
@@ -0,0 +1,15 @@
+package xyz.chasew.jacobsmmo;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class JacobsMMO extends JavaPlugin {
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
