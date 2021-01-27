6093b03714a8e99b450e3f8a2c7bd7021d0ffe34
Chase <72784986+aerglos@users.noreply.github.com>
2021-01-27 12:11:07 -0800

    main

--- src/xyz/chasew/jacobsmmo/commands/BackIfNear.java
+++ src/xyz/chasew/jacobsmmo/commands/BackIfNear.java
@@ -1,5 +1,7 @@
 package xyz.chasew.jacobsmmo.commands;
 
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.block.Block;
 import org.bukkit.block.CommandBlock;
@@ -9,11 +11,17 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
+import org.bukkit.util.Vector;
 
 import java.util.Collection;
 
 
 public class BackIfNear implements CommandExecutor {
+    public void pushBack(int x, int y, int z, Player player) {
+        Vector pushBackVector = player.getLocation().getDirection().normalize();
+        player.setVelocity(player.getVelocity().add(pushBackVector.multiply(-2)));
+
+    }
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (!(sender instanceof BlockCommandSender)) {
@@ -21,16 +29,19 @@ public class BackIfNear implements CommandExecutor {
             return true;
         }
         BlockCommandSender blockSender = (BlockCommandSender) sender;
-
+        if(args.length > 2) {
+            return false;
+        }
+        double radius = Double.parseDouble(args[1]);
         Block commandBlock = blockSender.getBlock();
         Location blockMiddle = commandBlock.getLocation().add(0.5, 0.5, 0.5);
-        double radius = 3;
+
         Collection<Entity> nearbyEntities = blockMiddle.getWorld().getNearbyEntities(blockMiddle, radius, radius, radius);
 
         for (Entity entity : nearbyEntities) {
             if (entity instanceof Player) {
                 Player hitPlayer = (Player) entity;
-                hitPlayer.sendMessage("You are near the CommandBlock @" + commandBlock.getX() + "|" + commandBlock.getY() + "|" + commandBlock.getZ());
+                pushBack(1, 2, 3, hitPlayer);
             }
         }
         return true;
