c07fb17220776651118b7b767235bbcce0341ecb
Chase <72784986+aerglos@users.noreply.github.com>
2021-01-27 11:03:49 -0800

    fix all

--- src/xyz/chasew/jacobsmmo/commands/EditItem.java
+++ src/xyz/chasew/jacobsmmo/commands/EditItem.java
@@ -1,6 +1,8 @@
 package xyz.chasew.jacobsmmo.commands;
 
+import com.sun.org.apache.xpath.internal.operations.Bool;
 import org.bukkit.ChatColor;
+import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandException;
@@ -9,8 +11,11 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 
-import java.util.Locale;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class EditItem implements CommandExecutor {
     @Override
@@ -18,6 +23,15 @@ public class EditItem implements CommandExecutor {
         if(sender instanceof Player) {
             Player playerSender = (Player) sender;
             ItemStack toEdit = playerSender.getInventory().getItemInMainHand();
+            ItemMeta toEditMeta = toEdit.getItemMeta();
+            if(toEditMeta == null) {
+                playerSender.sendMessage("You have to be holding an item to edit it.");
+            }
+            ItemStack materialAir = new ItemStack(Material.AIR, 1);
+            if(toEdit.equals(materialAir)) {
+                playerSender.sendMessage("You need to be holding something.");
+                return true;
+            }
             if(args.length < 1) {
                 playerSender.sendMessage("Please specify a value to edit");
                 return false;
@@ -29,6 +43,15 @@ public class EditItem implements CommandExecutor {
                         playerSender.sendMessage(ChatColor.GREEN + "Hey! You need an enchantment WITHOUT the 'minecraft:', level, and value (true or false). Example: '/edititem enchant efficiency 5 false' which would remove the efficiency 5 enchant.");
                         return true;
                     }
+
+                    //edititem enchant glint true
+                    /*
+                    if(args[1].equals("glint")) {
+                        if(Boolean.parseBoolean(args[2])) {
+                            toEdit.
+                        }
+                    }
+                     */
 
 
                     try {
@@ -43,6 +66,31 @@ public class EditItem implements CommandExecutor {
                         playerSender.sendMessage("Hey! That's not a valid enchant");
                         return false;
                     }
+                case "name":
+                    String fullCommand = String.join(" ", args);
+                    String searchPatternString = "(\".*\") (\".*\") (\".*\")";
+
+                    Pattern searchPattern = Pattern.compile(searchPatternString);
+                    Matcher matcher = searchPattern.matcher(fullCommand);
+
+                    if(!matcher.find()) {
+                        playerSender.sendMessage("Hey you need a new item name in between quotes! (NOF)");
+                        return true;
+                    }
+
+                    String newName = matcher.group(0).replace("\"", "").split(" ")[0];
+                    if(newName.equals("")) {
+                        playerSender.sendMessage("Hey you need a new item name in between quotes! (NOE)");
+                        return true;
+                    }
+                    toEditMeta.setDisplayName(ChatColor.translateAlternateColorCodes('&', newName));
+                    List<String> potentialLore = Arrays.asList(ChatColor.translateAlternateColorCodes('&', matcher.group(2).replace("\"", "")), ChatColor.translateAlternateColorCodes('&', matcher.group(3).replace("\"", "")));
+                    if(matcher.group(1) != null) {
+                        toEditMeta.setLore(potentialLore);
+                    }
+                    toEdit.setItemMeta(toEditMeta);
+                    playerSender.updateInventory();
+                    break;
                 default:
                     playerSender.sendMessage("That option isn't added yet!");
             }
