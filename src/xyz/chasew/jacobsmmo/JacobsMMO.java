a02bd9ca9df7cebf07e3f9f911cca76d425a21ec
Chase <72784986+aerglos@users.noreply.github.com>
2021-01-26 22:47:44 -0800

    Nightly 1

--- src/xyz/chasew/jacobsmmo/JacobsMMO.java
+++ src/xyz/chasew/jacobsmmo/JacobsMMO.java
@@ -1,11 +1,16 @@
 package xyz.chasew.jacobsmmo;
 
 import org.bukkit.plugin.java.JavaPlugin;
+import xyz.chasew.jacobsmmo.handlers.PlayerJoin;
+
+import java.util.List;
 
 public class JacobsMMO extends JavaPlugin {
     @Override
     public void onEnable() {
-
+        saveDefaultConfig();
+        getLogger().info("\n----\nJacob's MORPG Plugin is enabled\n----");
+        getServer().getPluginManager().registerEvents(new PlayerJoin(this), this);
     }
 
     @Override
---
+++ src/xyz/chasew/jacobsmmo/handlers/PlayerJoin.java
@@ -0,0 +1,24 @@
+package xyz.chasew.jacobsmmo.handlers;
+
+import org.bukkit.ChatColor;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.List;
+import java.util.Random;
+
+public class PlayerJoin implements Listener {
+    private final JavaPlugin thisPlugin;
+    public PlayerJoin(JavaPlugin thisPlugin) {
+        this.thisPlugin = thisPlugin;
+    }
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent joinEvent) {
+        List<String> joinMessages = thisPlugin.getConfig().getStringList("join-messages");
+        Random rand = new Random();
+        String randJoinMsg = joinMessages.get(rand.nextInt(joinMessages.size()));
+        joinEvent.setJoinMessage(ChatColor.GREEN + joinEvent.getPlayer().getName() + ChatColor.DARK_GREEN + " joined the server, " + randJoinMsg);
+    }
+}
