fd3e4a11f4bf91f2e42d990e8c78576077ec70a0
Chase <72784986+aerglos@users.noreply.github.com>
2021-01-27 16:26:52 -0800

    Refresh latest

--- src/xyz/chasew/jacobsmmo/handlers/BoomStickHandler.java
+++ src/xyz/chasew/jacobsmmo/handlers/BoomStickHandler.java
@@ -5,6 +5,7 @@ import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
@@ -20,7 +21,12 @@ public class BoomStickHandler implements Listener {
     @EventHandler
     public void onPlayerInteract(PlayerInteractEvent interactEvent) {
         Player player = interactEvent.getPlayer();
+        Action eventAction = interactEvent.getAction();
         if(player.getInventory().getItemInMainHand().equals(BoomStickRecipe.item)) {
+            if(eventAction == Action.RIGHT_CLICK_AIR || eventAction == Action.RIGHT_CLICK_BLOCK) {
+                interactEvent.setCancelled(true);
+                return;
+            }
             player.sendMessage(ChatColor.LIGHT_PURPLE + "BOOM!");
             player.addPotionEffect(new PotionEffect(PotionEffectType.DAMAGE_RESISTANCE, 175, 255));
             BackIfNear.pushAwayFrom(player, player.getLocation(), player.getLocation().add(0, 20, 0), 2.5);
